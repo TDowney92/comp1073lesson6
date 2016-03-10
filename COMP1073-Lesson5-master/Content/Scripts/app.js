3df4efddfed8405a24bc897da15ef9bc143f3a9e
TDowney92 <tmed425@hotmail.com>
2016-03-10 09:54:16 -0500

    Inital Commit

--- COMP1073-Lesson5-master/Content/Scripts/app.js
+++ COMP1073-Lesson5-master/Content/Scripts/app.js
@@ -4,70 +4,21 @@
 (function () {
 
 
-    var paragraphData = [{"paragraph":"First Paragraph Stuff"}, 
-                        {"paragraph":"Second Paragraph Stuff"},
-                        {"paragraph":"Third Paragraph Stuff"},
-                        {"paragraph":"Fourth Paragraph Stuff"}];
+    if (document.getElementById("about") != null) {
+        console.log("About Page");
+    } else if (document.getElementsById("project") != null) {
+        console.log("Projects Page");
+    } else if (document.getElementById("contact") != null) {
+        console.log("Contact Page");
+        var checkMeOut;
 
-    console.log("App Started..."); 
-  
-  /* the ugly way
-    console.log(paragraphData[1]);
-    console.log(paragraphData[1]);
-    console.log(paragraphData[2]);
-    console.log(paragraphData[0]);
-    console.log(paragraphData[5]);
-    console.log(paragraphData[6]);
-  */
-  
-    // a little nicer but counting up
-    
-    var paragraphDataLength = paragraphData.length;
-    for(var index= 0; index < paragraphDataLength; index++) {
-        console.log(paragraphData[index].paragraph);
+    checkMeOut = document.getElementById("checkMeOut");
+
+    checkMeOut.addEventListener("click", namedFunc);
+
+    function namedFunc() {
+        console.log("checkMeOut clicked");
     }
-    
-    var name = document.getElementById("name");
-    
-    var button = document.getElementById("Button1");
-    
-    button.addEventListener("click", function(){
-        // execute stuff here after button click
-        console.log("Clicked!!");
-        console.log(name.value);
-    });
-    
-   
-     // a little nicer but counting down
-    /*
-    for(var index= paragraphData.length-1; index > -1; index--) {
-        console.log(paragraphData[index]);
-    }
-    */
-    
-    
-    /* even nicer but more complicated
-    paragraphData.forEach(function(paragraph) {
-        console.log(paragraph);
-    });
-    */
-  
-    //declared a named function way
-
-    function replaceFirstParagraph() {
-        console.log("inside replaceFirstParagraph function");
-        var firstParagraph;
-
-        firstParagraph = document.getElementById("firstParagraph");
-
-        firstParagraph.innerHTML = "My New Paragraph Data";
-        
-    }
-
-   
-    // call the replaceFirstParagraph function
-    replaceFirstParagraph();
-
 
 })();
 
