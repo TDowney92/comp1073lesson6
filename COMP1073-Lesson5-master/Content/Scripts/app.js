3c1b09e521f32d48ee835332ad5f43e020f493e8
TDowney92 <tmed425@hotmail.com>
2016-03-10 09:57:58 -0500

    inital commit

--- COMP1073-Lesson5-master/Content/Scripts/app.js
+++ COMP1073-Lesson5-master/Content/Scripts/app.js
@@ -4,21 +4,5 @@
 (function () {
 
 
-    if (document.getElementById("about") != null) {
-        console.log("About Page");
-    } else if (document.getElementsById("project") != null) {
-        console.log("Projects Page");
-    } else if (document.getElementById("contact") != null) {
-        console.log("Contact Page");
-        var checkMeOut;
-
-    checkMeOut = document.getElementById("checkMeOut");
-
-    checkMeOut.addEventListener("click", namedFunc);
-
-    function namedFunc() {
-        console.log("checkMeOut clicked");
-    }
-
 })();
 
