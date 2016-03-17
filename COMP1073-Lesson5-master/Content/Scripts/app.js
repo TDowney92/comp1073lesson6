49f2031428f03b5832d44e7e8acbb412ce94e613
TDowney92 <tmed425@hotmail.com>
2016-03-17 09:07:07 -0400

    change lesson number

--- COMP1073-Lesson5-master/Content/Scripts/app.js
+++ COMP1073-Lesson5-master/Content/Scripts/app.js
@@ -3,6 +3,24 @@
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 
-
+    var myVariable = "hello";
+    console.log("myVariable" + myVariable);
+    
+    var myArray = [
+        "Tyler Downey",
+        "25 Rose Street",
+        "Barrie",
+        "Ontario",
+        23,
+        true
+    ];
+    
+    myArray.slice(4, 4)
+    
+    for(var index=0; index < myArray.length; index++) {
+        console.log(myArray[index]);
+    }
+    
+    
 })();
 
